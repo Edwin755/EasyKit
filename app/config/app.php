73633f0fdd98e70e2c26b610c1477a5831d21594
Edwin Dayot <edwin.dayot@sfr.fr>
2015-01-22 17:44:12 +0100

    App file

---
+++ app/config/app.php
@@ -0,0 +1,23 @@
+<?php
+
+    return array(
+
+        /**
+         * Session
+         */
+        'session_name'  => 'EasyKit_session',
+
+        /**
+         * Cookie
+         */
+        'cookie_name'   => 'EasyKit_cookie',
+        'cookie_time'   => 3600 * 24 * 28 * 3,
+
+        /**
+         * Security
+         */
+        'secure_key'    => 'o1Xc7m9KJ2S9X8307jFK67Fr',
+        'debug'         => true,
+
+
+    );
