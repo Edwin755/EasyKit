ee61a8fe53d7ac56bc6f15c1c4323978d3f339d0
Edwin Dayot <edwin.dayot@sfr.fr>
2015-02-04 15:57:22 +0100

    Favicon

--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="<?= HTML::link('default/stylesheets/style.css'); ?>">
     <link rel="stylesheet" href="<?= HTML::link('default/stylesheets/font-awesome-4.2.0/css/font-awesome.min.css'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1"/>
+    <link rel="icon" type="image/x-icon" href="<?= HTML::link('/favicon.ico') ?>">
     <link rel="canonical" href="<?= HTML::getCurrentURL(); ?>">
 </head>
 <body>
