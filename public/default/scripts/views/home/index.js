046c98bd7fcb602d9c04cd3d359d77f558239e83
Edwin Dayot <edwin.dayot@sfr.fr>
2015-02-10 14:33:47 +0100

    Show uniq event

--- public/default/scripts/views/home/index.js
+++ public/default/scripts/views/home/index.js
@@ -1,12 +1,11 @@
 var app = angular.module("myApp",[]);
 
 app.controller("popularEvents", function($scope, $http) {
-
+        
     $http.get(url + '/api/events?limit=6').
         success(function(data, status, headers, config) {
             if(typeof data === 'object'){
-                $scope.data = data;
-                $('.spinner').hide();
+                $scope.data = data;  
                 return;
             }else{
                 return;
@@ -15,26 +14,5 @@ app.controller("popularEvents", function($scope, $http) {
         .error(function(data, status, headers, config) {
           // log error
         });
-
-    $scope.like = function(e) {
-        var id = $(e.target).data('id');
-        $('ul[data-id="'+id+'"] .spinner-like').css('display','inline-block');
-        $('ul[data-id="'+id+'"] .like_number').hide();
-
-        $http.get(url+'/likes/create/'+id).
-            success(function(data, status, headers, config) {
-                if (data.success == true){
-                    nblike = $('ul[data-id="'+id+'"] .like_number').html();
-                    $('ul[data-id="'+id+'"] .like_number').html(parseInt(nblike)+1).show();
-                };
-
-                $('ul[data-id="'+id+'"] .like_number').show()
-                $('ul[data-id="'+id+'"] .spinner-like').hide();
-
-                return;
-            })
-            .error(function(data, status, headers, config) {
-              // log error
-            });
-    }
+    
 });
