dc8a8e67926aed362d99dbba7cae1f42cb7e4788
Edwin Dayot <edwin.dayot@sfr.fr>
2015-02-11 13:49:04 +0100

    Likes fix for COOKIE

--- core/Email.php
+++ core/Email.php
@@ -1,13 +1,24 @@
 <?php
+/**
+ * Email for the framework
+ *
+ * @author Edwin Dayot <edwin.dayot@sfr.fr>
+ * @copyright 2014
+ */
     
-    namespace Core;
-    
-    use \Swift_Message;
-    use \Swift_Attachment;
-    use \Swift_SmtpTransport;
-    use \Swift_Mailer;
-    
-    
+namespace Core;
+
+use Exception;
+use Swift_Attachment;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
+
+/**
+ * Class Email
+ *
+ * @package Core
+ */
 class Email
 {
     
@@ -19,7 +30,17 @@ class Email
      * @var array $from [email => name]
      */  
     private $to, $from, $subject, $content_text, $content_html, $attachment;
-    
+
+    /**
+     * Construct
+     *
+     * @param $to
+     * @param array $from
+     * @param $subject
+     * @param $content_html
+     * @param null $content_txt
+     * @param null $attachment
+     */
     function __construct($to, array $from, $subject, $content_html, $content_txt = null, $attachment = null)
     {
         $this->to = $to;        
@@ -29,44 +50,41 @@ class Email
         $this->content_txt = $content_txt;        
         $this->attachment = $attachment;        
     }
-            
+
+    /**
+     * Send
+     *
+     * @return bool
+     * @throws Exception
+     */
     function send()
     {
-    	$message = Swift_Message::newInstance();
-    
-    	//Objet
-    	$message->setSubject($this->subject);
-    
-        
-    	$message->setFrom($this->from);
-    
-    
-    	//Contenu
-    	$message->setBody($this->content_html);
-    
-    
-    	//Destinataire
+        $transport = Swift_SmtpTransport::newInstance('smtp.easykit.ovh', 587)
+            ->setUsername('hello@easykit.ovh')
+            ->setPassword('helloeemi');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance();
+    	$message
+            ->setSubject($this->subject)
+            ->setFrom($this->from)
+            ->setBody($this->content_html);
+
     	$message->setTo([$this->to]);
-    	
+
         if (!is_null($this->attachment) && !empty($this->attachment)) {
-    
-            foreach( $this->attachment as $attachment){
+            foreach($this->attachment as $attachment){
         		$message->attach(Swift_Attachment::fromPath($attachment));
             }
-        
     	}
-    	
-        $transport = Swift_SmtpTransport::newInstance('smtp.easykit.ovh', 587)
-		->setUsername('hello@easykit.ovh')
-		->setPassword('0h86c7qg');
 
     	$headers = $message->getHeaders();
-    	$mailer = Swift_Mailer::newInstance($transport);
-    
-    	if ($mailer->send($message)) {
-            return true;	
-        } else {
-            throw new Exception('Couldn\'t send the message');
+
+        try {
+            $mailer->send($message);
+        } catch (Exception $e) {
+            var_dump($e);
         }
+
+        return true;
     }
 }	
