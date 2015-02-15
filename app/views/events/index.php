fc8af95725367e17d501c24fa4c3563f68871b9e
Niko <nikoposner@gmail.com>
2015-02-15 11:51:35 +0100

    Likes

--- app/views/events/index.php
+++ app/views/events/index.php
@@ -13,32 +13,34 @@
             <div class="rect5"></div>
         </div>
 
-        <a class="block" ng-repeat="event in data.events track by $index" id="{{index}}" href="<?= HTML::link('/events/show/{{event.events_id}}')?>">
-            <div class="couverture">
-                <ul>
-                    <li ng-repeat="photos in event.events_medias|limitTo:1" style="background: url({{photos.medias_file}})"></li>
-                </ul>
-                <div class="description">
-                    {{event.events_summary}}<span class="info">Vancouver, November 24, 2014</span>
-                </div>
+            <a class="block" ng-repeat="event in data.events track by $index" id="{{event.events_id}}" href="<?= HTML::link('/events/show/{{event.events_id}}')?>">
+        <div class="couverture">
+            <ul>
+                <li ng-repeat="photos in event.events_medias|limitTo:1" style="background: url({{photos.medias_file}})"></li>
+            </ul>
+
+            <div class="description">
+                {{event.events_summary}}<span class="info">United States, May 13, 2015</span>
             </div>
+        </div>
 
-            <div class="block-text">
-                <p class="titre">{{event.events_name}}</p>
+        <div class="block-text">
+            <p class="titre">{{event.events_name}}</p>
 
-                <div class="trait-block-text"></div>
+            <div class="trait-block-text"></div>
 
-                <ul class="icones">
-                    <li><img src="<?= HTML::link('default/images/like.png'); ?>" data-id="{{event.events_id}}" class="like" title="Like this event" width="21" alt="like"> <span>{{event.events_like}}</span></li>
+            <ul class="icones" data-id="{{event.events_id}}">
+                <li ng-controller="likes">
+                    <img src="<?= HTML::link('default/images/like.png'); ?>" data-id="{{event.events_id}}" ng-click="like($event)" class="like off" title="Like this event" width="21" alt="like">
+                    <img src="<?= HTML::link('default/images/like_on.png'); ?>" data-id="{{event.events_id}}" ng-click="unLike($event)" class="like on" title="Like this event" width="21" alt="like">
 
-                    <li><img src="<?= HTML::link('default/images/share.png'); ?>" title="Share this event" width="21" alt="share"></li>
+                    <div class="spinner-like"></div>
+                    <span class="like_number">{{event.events_like}}</span>
+                </li>
 
-                    <li class="favorit">
-                        <img class="coeur" src="<?= HTML::link('default/images/favorit.png'); ?>" title="Add to your favorite events" width="23" alt="favorit">
-                        <img class="coeur2" src="<?= HTML::link('default/images/favorit-vert.png'); ?>" width="23" alt="favorit2">
-                    </li>
-                </ul>
-            </div>
-        </a>
-    </div>
+                <li><img src="<?= HTML::link('default/images/share.png'); ?>" title="Share this event" width="21" alt="share"></li>
+            </ul>
+        </div>
+    </a>
+        </div>
 </div>
--- public/default/scripts/views/events/index.js
+++ public/default/scripts/views/events/index.js
@@ -4,6 +4,45 @@ app.controller("allEvents", function($scope, $http) {
         success(function(data, status, headers, config) {
             if (typeof data === 'object') {
                 $scope.data = data;
+                var events = data.events;
+                
+                $http.get(url + '/likes/user').success(function(data, status, headers, config) {
+                if (typeof data === 'object') {
+                    connect = data.authed;
+                    userLikes = data.likes;
+                    
+                    console.log(userLikes);
+                
+                events.forEach(function(event){
+                     
+                    eventid = event.events_id;
+                    
+                    userLikes.forEach(function(entry) { 
+                        
+                        if(eventid == entry){
+                            nblike = $('#' + eventid + ' .like_number').html();
+                            
+                            $('#' + eventid + ' .like.on').show();
+                            $('#' + eventid + ' .like.off').hide();
+                            
+                            if(connect == false){
+                                $('#' + eventid + ' .like_number').html(parseInt(nblike)+1).show();
+                            }
+                        }
+
+                    });
+                });
+                    
+                    return;
+                    
+                } else {
+                    return;
+                }
+            })
+            .error(function(data, status, headers, config) {
+              // log error
+            });
+                
                 $('.spinner').hide();
                 return;
             } else {
