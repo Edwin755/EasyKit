9a297ad5a454bdbaff5cf872ff66cfb00a30a5a2
Edwin Dayot <edwin.dayot@sfr.fr>
2015-02-27 09:34:36 +0100

    Packs linked to Events

---
+++ app/models/Events_packs.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: H3yden
+ * Date: 10/12/2014
+ * Time: 12:05
+ */
+
+namespace App\Models;
+
+use Core\Model;
+
+/**
+ * Class Events_Packs
+ *
+ * @package App\Models
+ */
+class Events_packs extends Model
+{
+
+
+}
